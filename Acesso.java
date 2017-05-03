cafd9ab79f9d75275be01a8894e12f238c094341
Jessica Cristina <jessica.cris1127@gmail.com>
2017-05-03 18:04:05 -0300

    Questão 2
    Signed-off-by: Jessica Cristina <jessica.cris1127@gmail.com>

--- Acesso.java
+++ Acesso.java
@@ -3,7 +3,7 @@ public class Acesso {
 
 	public static final float VALOR_FRACAO = (float) 2.0;
 	public static final float VALOR_HORA = (float) 7.0;
-	public static final float VALOR_DIARIA = (float) 30.0;
+	public static final Class VALOR_DIARIA = (float) 30.0;
 	
 	public String placa;
 	public int dia, mes, ano;
@@ -11,52 +11,51 @@ public class Acesso {
 	           minutosEntrada;
 	public int horaSaida, 
 			   minutosSaida;
-	
 
 
 	public Acesso() {}
 
 	
-	public Acesso(int dia, int mes, int ano, int horaEntrada, int minutosEntrada) { 
-		this.dia = dia;
-		this.mes = mes;
-		this.ano = ano;
-		this.horaEntrada = horaEntrada;
-		this.minutosEntrada = minutosEntrada;
+	 Acesso(int horaSaidaArg, int horaEntradaArg, int minutosEntradaArg, int minutosSaidaArg){
+	      horaEntrada = horaEntradaArg;
+	      horaSaida = horaSaidaArg;
+	      minutosEntrada = minutosEntradaArg;
+	      minutosSaida = minutosSaidaArg;
+	    }
+
+	    Class calculaValor(){
+
+	        int quantidadeHoras;
+	        int quantidadeMinutos;
+	        
+
+	     if (horaSaida == horaEntrada)
+	       quantidadeMinutos = minutosSaida - minutosEntrada;
+	     else if (horaSaida > horaEntrada && minutosEntrada == minutosSaida){
+	       quantidadeMinutos = 0;
+	       quantidadeHoras = horaSaida - horaEntrada;
+	     }
+	     else if (horaSaida > horaEntrada && minutosEntrada > minutosSaida)
+	       quantidadeMinutos = minutosSaida - minutosEntrada;
+	     else if (horaSaida > horaEntrada && minutosSaida < minutosEntrada){
+	       quantidadeMinutos = minutosSaida + (60 - minutosEntrada);
+	       quantidadeHoras = horaSaida - horaEntrada - 1;
+	     }
+	     else {
+	       quantidadeHoras = 0;
+	       quantidadeMinutos = 0;
+	     }
+	     
+	     Class valorTotal = quantidadeHoras * VALOR_HORA + Math.ceil(quantidadeMinutos / 15.0) * VALOR_FRACAO;
+
+	     float comparaValor(){
+	    	 
+	     if (calculaValor.valorTotal >=9)
+	       return VALOR_DIARIA;
+	     else
+	       return valorTotal;
+	    }
 	}
-	
-	
-	public float calcularValor() { 
-		int quantidadeHoras = horaSaida - horaEntrada; 
-		int quantidadeMinutos; 
-		
-		if (horaSaida == horaEntrada)
-			quantidadeMinutos = minutosSaida - minutosEntrada;
-		else if (horaSaida > horaEntrada && minutosEntrada == minutosSaida){
-			quantidadeMinutos = 0;
-			quantidadeHoras = horaSaida - horaEntrada;
-		}
-		else if (horaSaida > horaEntrada && minutosEntrada > minutosSaida) 
-			quantidadeMinutos = minutosSaida - minutosEntrada;
-		else if (horaSaida > horaEntrada && minutosSaida < minutosEntrada){
-			quantidadeMinutos = minutosSaida + (60 - minutosEntrada);
-			quantidadeHoras = horaSaida - horaEntrada - 1;
-		}
-		else {
-			quantidadeHoras = 0;
-			quantidadeMinutos = 0;
-		}
-		
-		float valorTotal = 0; 
-		valorTotal += quantidadeHoras * VALOR_HORA;
-		valorTotal += Math.ceil(quantidadeMinutos / 15.0) * VALOR_FRACAO;		
-		
-		if (quantidadeHoras >=9)
-			return VALOR_DIARIA;
-		else 
-			return valorTotal;
-	}
-	
 	
 	public void setHoraSaida(int horaSaida) {
 		this.horaSaida = horaSaida;
